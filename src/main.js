6010215c6f5a960270be77ac0be1459ef2b1e893
mjjung5 <mjjung5@lotte.net>
2020-10-27 17:48:20 +0900

    2010-10-27

--- src/main.js
+++ src/main.js
@@ -1,7 +1,15 @@
 import Vue from "vue";
 import App from "./App.vue";
+import common from "./css/common.scss";
+import layouts from "./css/layout.scss";
+import mainVisual from "./css/main.scss";
+import reset from "./css/reset.scss";
 
 Vue.config.productionTip = false;
+Vue.use(common);
+Vue.use(layouts);
+Vue.use(mainVisual);
+Vue.use(reset);
 
 new Vue({
   render: h => h(App)
